6bd591ce120b17a933d745ec9cfae0e1b780d2ac
zkfy <lib4net@163.com>
2021-03-08 16:36:51 +0800

    优化项目

--- mgrserver/dev.go
+++ mgrserver/dev.go
@@ -22,7 +22,7 @@ func init() {
 	hydra.Conf.API("8091").Header(header.WithCrossDomain())
 	hydra.Conf.Vars().DB().MySQL("db", "hydra", "123456", "192.168.0.36:3306", "hydra", db.WithConnect(20, 10, 600))
 	// hydra.Conf.Vars().DB().MySQL("db", "hydra", "123456", "222.209.84.37:10036", "hydra", db.WithConnect(20, 10, 600))
-	// hydra.Conf.Vars().DB().MySQLByConnStr("db", "scoresys:1qaz2wsxA@tcp(rm-bp1jncd360df9dqvyro.mysql.rds.aliyuncs.com:3306)/scoresys?charset=utf8")
+	// hydra.Conf.Vars().DB().MySQLByConnStr("db", "scoresys:a1qaz2wsxA@tcp(rm-abp1jncd360df9dqvyro.mysql.rds.aliyuncs.com:3306)/scoresys?charset=utf8")
 
 	//启动时参数配置检查
 	App.OnStarting(func(appConf app.IAPPConf) error {
